dc7b8b9d4036106c45d16a1f78870c456b1cca79
trexxet <atomnik07@gmail.com>
2026-03-03 03:19:42 +0500

    Add PrettyEnum::size(), has_idx(), Meta
    - rework PrettyEnum test

--- lib/include/Basedlib/PrettyEnum.hpp
+++ lib/include/Basedlib/PrettyEnum.hpp
@@ -5,33 +5,41 @@
 #include <string_view>
 #include <utility>
 
-#include "Basedlib/Meta.hpp"
+#include "Meta.hpp"
 
 /* This would work only for enums that are:
  * 1) Dense (values go from 0 to N - 1)
- * 2) Have the last value COUNT = N
+ * 2) Have the last value _bl_count = N
  */
 
-#define Bl_PrettyEnum(name, ...) enum class name { __VA_ARGS__, COUNT }
+#define Bl_PrettyEnum(name, ...) enum class name { __VA_ARGS__, _bl_count }
 
 namespace Basedlib {
 
 template <typename T>
-concept PrettyEnumT = std::is_scoped_enum_v<T> && requires { T::COUNT; };
+concept PrettyEnumT = std::is_scoped_enum_v<T> && requires { T::_bl_count; };
 
-template <PrettyEnumT T, bool scoped = false>
-class PrettyEnum {
-public:
+template <PrettyEnumT T>
+struct PrettyEnum {
+	using Meta = Meta<T>;
+
 	static constexpr size_t idx (T i) { return static_cast<size_t> (i); }
 	static constexpr T val (size_t i) { return static_cast<T> (i); }
+	static constexpr size_t size () { return idx (T::_bl_count); }
+	static constexpr bool has_idx (size_t i) { return i < size(); }
 
 private:
-	static constexpr auto names = [] <std::size_t... I> (std::index_sequence<I...>) {
-		return std::array { Meta<T>::template v_name<val (I), scoped>()... };
-	} (std::make_index_sequence <idx (T::COUNT)>());
+	template<bool scoped>
+	static constexpr auto get_names = [] <std::size_t... I> (std::index_sequence<I...>) {
+		return std::array { Meta::template v_name<val (I), scoped>()... };
+	};
+	static constexpr auto idx_sequence = std::make_index_sequence <size()> ();
+	static constexpr auto names = get_names<false> (idx_sequence);
+	static constexpr auto scoped_names = get_names<true> (idx_sequence);
 
 public:
 	static constexpr std::string_view to_string (T value) { return names[idx (value)]; }
+	static constexpr std::string_view to_scoped_string (T value) { return scoped_names[idx (value)]; }
 };
 
 }
--- test/testPrettyEnum.cpp
+++ test/testPrettyEnum.cpp
@@ -1,8 +1,12 @@
+#include "Basedlib/Meta.hpp"
 #include "Basedlib/PrettyEnum.hpp"
 
 #include <print>
 
-Bl_PrettyEnum (Color, Red, Green, Blue );
+Bl_PrettyEnum (Color, Red, Green, Blue);
+
+using PrettyColor = Basedlib::PrettyEnum<Color>;
+using MetaColor = PrettyColor::Meta; // same as Basedlib::Meta<Color>;
 
 /* Meta can be used for compile-time values only, but for many types (I suppose).
  * PrettyEnum can be used for dense enums created with Bl_PrettyEnum both in 
@@ -12,25 +16,18 @@ Bl_PrettyEnum (Color, Red, Green, Blue );
 int main () {
 	Color c;
 
-	int ch = std::getchar();
-	switch (ch) {
-		case '2': c = Color::Green; break;
-		case '3': c = Color::Blue; break;
-		default: c = Color::Red; break;
+	int input = std::getchar() - '0';
+	if (PrettyColor::has_idx (input)) {
+		c = PrettyColor::val (input);
+	} else {
+		std::print ("Wrong input index!\n");
+		return 0;
 	}
 
-	std::print ("Runtime {}: {}\n",
-		Basedlib::Meta<Color>::t_name(),
-		Basedlib::PrettyEnum<Color>::to_string(c)
-	);
-
-	std::print ("Static and scoped: {}\n",
-		Basedlib::PrettyEnum<Color, true>::to_string(Color::Red)
-	);
-
-	std::print ("Another static but unscoped: {}\n",
-		Basedlib::Meta<Color>::template v_name<Color::Green>()
-	);
+	std::print ("Type: {}, size: {}\n", MetaColor::t_name(), PrettyColor::size());
+	std::print ("Runtime scoped/unscoped: {}, {}\n", PrettyColor::to_scoped_string(c), PrettyColor::to_string(c));
+	std::print ("Static scoped/unscoped using PrettyColor: {}, {}\n", PrettyColor::to_scoped_string(Color::Red), PrettyColor::to_string(Color::Red));
+	std::print ("Same but using MetaColor: {}, {}\n", MetaColor::v_name<Color::Green, true>(), MetaColor::v_name<Color::Green>());
 
 	return 0;
 }
