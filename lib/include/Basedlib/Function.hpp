21ed01e501e1cbf18ae4a8597b14f96bef730077
trexxet <atomnik07@gmail.com>
2026-04-05 02:42:21 +0500

    Add FunctionRef

--- lib/include/Basedlib/Function.hpp
+++ lib/include/Basedlib/Function.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <concepts>
 #include <type_traits>
 #include <utility>
@@ -7,20 +8,23 @@
 namespace Basedlib {
 
 // TODO: use std::function_ptr when available
-template<typename Signature> requires std::is_function_v<Signature>
+
+/// @brief Function is a compile-time nullable function pointer wrapper, also
+/// capable of handling non-capturing lambdas.
+template <typename Signature> requires std::is_function_v<Signature>
 class Function;
 
-template<typename Ret, typename... Args>
-struct Function<Ret(Args...)> {
-	using Signature = Ret(Args...);
+template <typename Ret, typename... Args>
+struct Function <Ret (Args...)> {
+	using Signature = Ret (Args...);
 
 	Signature* ptr = nullptr;
 
 	constexpr Function () = default;
-	template<typename F> requires std::is_convertible_v<F, Signature*>
+	template<typename F> requires std::is_convertible_v<std::decay_t<F>, Signature*>
 	constexpr Function (F fn) noexcept : ptr (fn) { }
 
-	template<typename F> requires std::is_convertible_v<F, Signature*>
+	template<typename F> requires std::is_convertible_v<std::decay_t<F>, Signature*>
 	constexpr Function& operator= (F fn) noexcept { ptr = fn; return *this; }
 
 	constexpr Ret operator() (Args... args) const {
@@ -29,6 +33,34 @@ struct Function<Ret(Args...)> {
 	}
 
 	constexpr explicit operator bool () const noexcept { return ptr != nullptr; }
+};
+
+/// @brief FunctionRef is similar to Function, but non-nullable.
+template <typename Signature> requires std::is_function_v<Signature>
+class FunctionRef;
+
+template <typename Ret, typename... Args>
+struct FunctionRef <Ret (Args...)> {
+	using Signature = Ret (Args...);
+
+	Signature* ptr;
+
+	FunctionRef () = delete;
+	FunctionRef (std::nullptr_t) = delete;
+	FunctionRef& operator= (std::nullptr_t) = delete;
+
+	template<typename F>
+	requires std::is_convertible_v<std::decay_t<F>, Signature*> && (!std::same_as<std::decay_t<F>, std::nullptr_t>)
+	constexpr FunctionRef (F fn) noexcept : ptr (fn) { assert (ptr); }
+
+	template<typename F>
+	requires std::is_convertible_v<std::decay_t<F>, Signature*> && (!std::same_as<std::decay_t<F>, std::nullptr_t>)
+	constexpr FunctionRef& operator= (F fn) noexcept { ptr = fn; assert (ptr); return *this; }
+
+	constexpr Ret operator() (Args... args) const {
+		if constexpr (std::is_void_v<Ret>) ptr (std::forward<Args> (args)...);
+		else return ptr (std::forward<Args> (args)...);
+	}
 };
 
 }
