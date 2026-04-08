e6fe1c73230fa1ba2d12f8177492a02c98406925
trexxet <atomnik07@gmail.com>
2026-04-09 03:17:59 +0500

    RingBuffer: switch test to Basedtest

--- test/testRingBuffer.cpp
+++ test/testRingBuffer.cpp
@@ -1,26 +1,51 @@
 #include "Basedlib/RingBuffer.hpp"
+#include "Basedtest/Suite.hpp"
 
-#include <cstdio>
-#include <print>
+const size_t capacity = 3;
 
-using Rb = Basedlib::RingBuffer<int>;
+BT_SCENARIO_TEST (test_rb_create) {
+	Basedlib::RingBuffer<int> rb (capacity);
 
-void print_rb (const Rb& rb) {
-	for (int x : rb)
-		std::print ("{} ", x);
-	std::putchar ('\n');
+	BT_ASSERT (rb.capacity() == capacity);
+	BT_ASSERT (rb.size() == 0);
+	BT_ASSERT (!rb.full());
+	BT_ASSERT (rb.empty());
+	BT_SUCCESS;
+}
+
+BT_SCENARIO_TEST (test_rb_append_no_wrap) {
+	Basedlib::RingBuffer<int> rb (capacity);
+	rb.push (1);
+	rb.push (2);
+
+	BT_ASSERT (rb.size() == 2);
+	BT_ASSERT (!rb.full());
+	BT_ASSERT (!rb.empty());
+	BT_ASSERT (rb[0] == 1);
+	BT_ASSERT (rb[1] == 2 && rb.back() == rb[1]);
+	BT_SUCCESS;
+}
+
+BT_SCENARIO_TEST (test_rb_append_wrap) {
+	Basedlib::RingBuffer<int> rb (capacity);
+	rb.push (1);
+	rb.push (2);
+	rb.push (3);
+	rb.push (4);
+
+	BT_ASSERT (rb.size() == capacity);
+	BT_ASSERT (rb.full());
+	BT_ASSERT (!rb.empty());
+	BT_ASSERT (rb[0] == 2);
+	BT_ASSERT (rb[1] == 3);
+	BT_ASSERT (rb[2] == 4 && rb.back() == rb[2]);
+	BT_SUCCESS;
 }
 
 int main () {
-	Rb rb (3);
-
-	rb.push (1);
-	rb.push (2);
-	print_rb (rb);
-
-	rb.push (3);
-	rb.push (4);
-	print_rb (rb);
-
-	return 0;
+	return Basedtest::Suite ("testRingbufferSuite", tests (
+		BT_SUITE_SCENARIO (test_rb_create),
+		BT_SUITE_SCENARIO (test_rb_append_no_wrap),
+		BT_SUITE_SCENARIO (test_rb_append_wrap)
+	)).run().rc();
 }
