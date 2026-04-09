4dd2c31bd9d677a5c01e7b52c38e525b5a5d76fc
trexxet <atomnik07@gmail.com>
2026-04-10 03:59:40 +0500

    Basedtest: support ADL to_string() in format_value()

--- lib/include/Basedtest/Common.hpp
+++ lib/include/Basedtest/Common.hpp
@@ -11,6 +11,8 @@ std::string format_value (const T& val) {
 		return std::format ("{}", val);
 	else if constexpr (requires { val.to_string(); })
 		return std::string (val.to_string());
+	else if constexpr (requires { to_string (val); })
+		return std::string (to_string (val));
 	else return "<unprintable>";
 }
 
