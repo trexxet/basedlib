d1b049c2054e64692814d8d822f68b29bcf1718d
trexxet <atomnik07@gmail.com>
2026-04-10 01:43:52 +0500

    Add demos
    - copy testFSM to demoFSM
    - copy testTest to demoTest
    - copy testPrettyEnum to demoPrettyEnum

---
+++ demo/demoFSM.cpp
@@ -0,0 +1,113 @@
+#include "Basedlib/FSM.hpp"
+
+#include <cstdio>
+#include <print>
+
+enum class States {
+	ST_A,
+	ST_B,
+	ST_C,
+	ST_D
+};
+
+enum class Events {
+	EV_A, // States: A->B, B->C, C->D, D not permitted
+	EV_B  // States: A->A (ignored), B->A, C->B, D->C
+};
+
+struct Context {
+	size_t evA_count = 0;
+	size_t evB_count = 0;
+};
+
+/* Note that callbacks won't have a Context argument if Context is nullptr_t (as by default).
+ * If enums has > 32 items, the range should be increased in the Enum template arg (as in PrettyEnum).
+ */
+using FSM = Basedlib::FSM::FSM <Basedlib::FSM::Enum<States>, Basedlib::FSM::Enum<Events>, Context,
+	[] (std::string_view s) { std::print ("FSM Logger: {}\n", s); }
+>;
+/* This also creates the following type aliases:
+ * FSM::State = States
+ * FSM::Event = Events
+ * FSM::EventCallbackResult = std::optional<States>
+ * FSM::StateCallback = void (Context*) or void ()
+ * FSM::EventCallback = EventCallbackResult (FSM*, Context*) or EventCallbackResult (FSM*)
+ * Also, the following const is declared:
+ * FSM::EventNotPermitted = std::nullopt
+ */
+
+void print_callback (std::string_view str) { std::print ("Callback: {}\n", str); }
+
+// Event callback as a separate function
+FSM::EventCallbackResult fsm_ev_a (FSM* fsm, Context* ctx) {
+	print_callback ("event EV_A");
+	ctx->evA_count++;
+
+	States newState = fsm->state();
+	switch (fsm->state()) {
+		case States::ST_A: newState = States::ST_B; break;
+		case States::ST_B: newState = States::ST_C; break;
+		case States::ST_C: newState = States::ST_D; break;
+		case States::ST_D: default: return FSM::EventNotPermitted;
+	}
+
+	fsm->switch_state (newState);
+	return newState;
+}
+
+// State callbacks as separate functions
+void fsm_st_b_enter (Context *ctx) { print_callback ("enter ST_B"); }
+void fsm_st_b_exit  (Context *ctx) { print_callback ("exit ST_B"); }
+
+int main () {
+	Context ctx {};
+
+	FSM fsm (States::ST_A, &ctx, FSM::make_callbacks (
+		// State callbacks as non-capturing lambdas
+		FSM::state_cb <States::ST_A> ({
+			.on_enter = [] (Context* ctx) { print_callback ("enter ST_A"); },
+			.on_exit  = [] (Context* ctx) { print_callback ("exit ST_A"); }
+		}),
+		FSM::state_cb <States::ST_B> ({.on_enter = fsm_st_b_enter, .on_exit = fsm_st_b_exit}),
+		// Callbacks can be safely omitted
+		FSM::state_cb <States::ST_C> ({.on_enter = [] (Context* ctx) { print_callback ("enter ST_C"); }}),
+		FSM::event_cb <Events::EV_A> (fsm_ev_a),
+		// Event callback as a non-capturing lambda
+		FSM::event_cb <Events::EV_B> ([] (FSM* fsm, Context* ctx) -> FSM::EventCallbackResult {
+			print_callback ("event EV_B");
+			ctx->evB_count++;
+
+			States newState = fsm->state();
+			switch (fsm->state()) {
+				case States::ST_B: newState = States::ST_A; break;
+				case States::ST_C: newState = States::ST_B; break;
+				case States::ST_D: newState = States::ST_C; break;
+				default: break;
+			}
+
+			fsm->switch_state (newState);
+			return newState;
+		})
+	));
+
+	auto trigger_event = [&fsm] (Events ev) {
+		if (!fsm.event (ev))
+			std::print ("event({}) returned false (= std::nullopt = FSM::EventNotPermitted)\n", Basedlib::PrettyEnum<Events>::to_string (ev));
+	};
+
+	std::print ("Enter 1 to trigger EV_A, 2 to trigger EV_B, any other input to stop\n");
+	bool running = true;
+	while (running) {
+		int input = std::getchar();
+		if (input == '\n') continue;
+		switch (input - '0') {
+			case 1: trigger_event (Events::EV_A); break;
+			case 2: trigger_event (Events::EV_B); break;
+			default: running = false; break;
+		}
+	}
+
+	std::print ("Event count EV_A: {} EV_B: {}\n", ctx.evA_count, ctx.evB_count);
+
+	return 0;
+}
---
+++ demo/demoTest.cpp
@@ -0,0 +1,107 @@
+#include "Basedtest/Suite.hpp"
+
+#include <cassert>
+#include <expected>
+#include <format>
+#include <string>
+
+struct Point {
+	int x, y;
+
+	// To be used in value tests as output, the type must satisfy std::equality_comparable
+	bool operator== (const Point&) const = default;
+
+	// To be printable in a suite run, the type must satisfy std::formattable
+	// or have std::string to_string() method
+	std::string to_string() const {
+		return std::format ("{{x = {}, y = {}}}", x, y);
+	}
+};
+
+// 1) Multiple value test cases for the same function
+Point mirror (const Point& a) { return { -a.x, -a.y }; }
+int test_mirror () {
+	// Note that suite initialization is consteval
+	constexpr Point pointA {-1337, -1337};
+	constexpr Point mirror_pointA {1337, 1337};
+	Basedtest::Suite testMirror ("testMirror", Basedtest::cases <mirror> (
+		// Cases can be constructed with designated initializers
+		Basedtest::ValueCase {.name = "{1, 1}", .input = Point {1, 1}, .expected = Point {-1, -1}},
+		Basedtest::ValueCase {"{2, -3}", Point {2, -3}, Point {-2, 3}},
+		Basedtest::ValueCase {"{-1337, -1337}", pointA, mirror_pointA}
+	));
+	// run<true> (default) means that the suite will print the result to the stdout
+	Basedtest::SuiteFails testMirrorFails = testMirror.run();
+	// rc() returns 0 if SuiteFails is empty
+	return testMirrorFails.rc();
+}
+
+// 2) Multiple assertion test cases for the same BT_ASSERT_TEST function
+BT_ASSERT_TEST (tester_point_coord_sign, Point, d) {
+	BT_ASSERT (d.x == -d.y);
+	BT_SUCCESS;
+}
+Basedtest::SuiteFails test_point_coord_sign () {
+	Basedtest::Suite testPointCoordSign ("testPointCoordSign", Basedtest::cases <tester_point_coord_sign> (
+		Basedtest::AssertCase {.name = "{-1, 1}", .input = Point {-1, 1}},
+		Basedtest::AssertCase {"{2, 3}", Point {2, 3}} // this one would fail!
+	));
+	// run<false> is a silent run
+	Basedtest::SuiteFails fails = testPointCoordSign.run<false>();
+	assert (fails); // SuiteFails is true when not empty
+	return fails;
+}
+
+// 3) Mixed test suite for different functions and even test types
+BT_SCENARIO_TEST (tester_point_coord_compare) {
+	Point p {-1, 2};
+	// Assert/Scenario test fail can be triggered manually
+	if (p.y < p.x) BT_FAIL ("Wrong coordinates in p!");
+	BT_ASSERT (p.x != p.y);
+	BT_SUCCESS;
+}
+int inc (const int& x) { return x + 1; }
+int test_mixed () {
+	using namespace Basedtest;
+	return Suite ("testMixed", tests (
+		ValueTest {"inc1", 1, 2, inc}, // ValueTests and AssertTests can't be constructed with designated initializers as they rely on CTAD
+		ValueTest {"dec2", 2, 1, [] (const int& x) { return x - 1; } }, // Lambdas can be used
+		AssertTest {"point1", Point {1, -1}, tester_point_coord_sign},
+		ScenarioTest {"point2", tester_point_coord_compare},
+		BT_SUITE_SCENARIO (tester_point_coord_compare) // Sugar macro to make a scenario with name equal to function name
+	)).run().rc();
+}
+
+// 4) Tests can be run without suite. In that case ValueTest can return mismatched values.
+int test_single () {
+	using namespace Basedtest;
+	const std::string name ("testSinglePoint");
+	ValueTest test {
+		name, Point {1, 2}, Point {21, 1},
+		[] (const Point& p) { return Point {p.y, p.x}; } // would fail
+	};
+	ValueTestResult result = test.run();
+	if (!result)
+		std::print (
+			"{} failed on purpose: expected '{}', got '{}'\n",
+			name, result.error().expected.to_string(), result.error().got.to_string()
+		);
+	return 0;
+}
+
+int main () {
+	// This macro would return the int passed if != 0
+	BT_CHECK_RC (test_mirror());
+
+	auto fails = test_point_coord_sign();
+	for (const Basedtest::SuiteFail& fail : fails) {
+		std::print ("Iterating over fails: '{}' failed on purpose with '{}'\n", fail.testName, fail.msg);
+	}
+
+	int rcMixed = test_mixed();
+	BT_CHECK_RC (rcMixed);
+
+	BT_CHECK_RC (test_single());
+
+	return 0;
+}
