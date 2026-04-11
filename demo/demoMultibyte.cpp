37d7e8b4bcaf46a7e57ccf08d4d67e5ff84eec2e
trexxet <atomnik07@gmail.com>
2026-04-12 03:45:22 +0500

    Test: switch Multibyte test to Basedtest
    - move old Multibyte test to demos
    - CMake: move add_test to add_basedlib_test

---
+++ demo/demoMultibyte.cpp
@@ -0,0 +1,36 @@
+#include "Basedlib/Text/Multibyte.hpp"
+#include "Basedlib/Text/Widechar.hpp"
+
+#include <cstdio>
+#include <print>
+#include <string>
+
+#if defined(__WIN32)
+# define RESET_STDOUT_ORIENTATION()
+#else
+# define RESET_STDOUT_ORIENTATION() { std::freopen (nullptr, "w", stdout); }
+#endif
+
+int main () {
+#if !defined(__WIN32)
+	std::setlocale (LC_ALL, "C.utf8");
+#endif
+
+	// UTF-8 string
+	const std::string mbstr0 = "Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
+	std::print ("{}", mbstr0);
+
+	// Convert UTF-8 string to wchar string and print
+	RESET_STDOUT_ORIENTATION();
+	std::wstring wcstr = Basedlib::Text::mbs_to_wcs (mbstr0);
+	Basedlib::Text::print_wcs (wcstr);
+
+	// Convert wchar string back to UTF-8 string
+	RESET_STDOUT_ORIENTATION();
+	std::string mbstr1 = Basedlib::Text::wcs_to_mbs (wcstr);
+	std::print ("{}", mbstr1);
+
+	std::print ("End!");
+
+	return 0;
+}
--- test/testMultibyte.cpp
+++ test/testMultibyte.cpp
@@ -1,33 +1,40 @@
 #include "Basedlib/Text/Multibyte.hpp"
-#include "Basedlib/Text/Widechar.hpp"
+#include "Basedtest/Suite.hpp"
 
-#include <cstdio>
-#include <print>
+#if !defined(__WIN32)
+#include <clocale>
+#endif
 #include <string>
 
-#if defined(__WIN32)
-# define RESET_STDOUT_ORIENTATION()
-#else
-# define RESET_STDOUT_ORIENTATION() { std::freopen (nullptr, "w", stdout); }
-#endif
+using namespace Basedtest;
+
+BT_SCENARIO_TEST (test_mbs_to_wcs) {
+	const std::string in = "Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
+	const std::wstring expected = L"Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
+
+	std::wstring got = Basedlib::Text::mbs_to_wcs (in);
+	BT_ASSERT_EQ (got, expected);
+
+	BT_SUCCESS;
+}
+
+BT_SCENARIO_TEST (test_wcs_to_mbs) {
+	const std::wstring in = L"Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
+	const std::string expected = "Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
+
+	std::string got = Basedlib::Text::wcs_to_mbs (in);
+	BT_ASSERT_EQ (got, expected);
+
+	BT_SUCCESS;
+}
 
 int main () {
 #if !defined(__WIN32)
 	std::setlocale (LC_ALL, "C.utf8");
 #endif
 
-	const std::string mbstr0 = "Hello, World! Привет, мир! 你好, 世界! 𒁲 𒄳 🚀\n";
-	std::print ("{}", mbstr0);
-
-	RESET_STDOUT_ORIENTATION();
-	std::wstring wcstr = Basedlib::Text::mbs_to_wcs (mbstr0);
-	Basedlib::Text::print_wcs (wcstr);
-
-	RESET_STDOUT_ORIENTATION();
-	std::string mbstr1 = Basedlib::Text::wcs_to_mbs (wcstr);
-	std::print ("{}", mbstr1);
-
-	std::print ("End!");
-
-	return 0;
+	return Suite ("Multibyte", tests (
+		BT_SUITE_SCENARIO (test_mbs_to_wcs),
+		BT_SUITE_SCENARIO (test_wcs_to_mbs)
+	)).run_rc();
 }
